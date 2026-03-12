c32ef60819fa88e8ac2b9b7dc503ba4d00d7c7e9
Senechal jf <jfsenechal@gmail.com>
2026-03-12 11:30:33 +0100

    remove prefix circuit-court from routes

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,11 @@ use App\Livewire\CircuitCourt\LocaliteShow;
 use App\Livewire\CircuitCourt\MapPage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => redirect('circuit-court'));
-
-// Circuit Court
-Route::prefix('circuit-court')->group(function (): void {
-    Route::get('/', MapPage::class)->name('circuit-court.map');
-    Route::get('/acteur', ActeurIndex::class)->name('circuit-court.acteurs');
-    Route::get('/acteur/{shop:slug}', ActeurShow::class)->name('circuit-court.acteur.show');
-    Route::get('/filiere', FiliereIndex::class)->name('circuit-court.filieres');
-    Route::get('/filiere/{tag:slug}', FiliereShow::class)->name('circuit-court.filiere.show');
-    Route::get('/localite', LocaliteIndex::class)->name('circuit-court.localites');
-    Route::get('/localite/{city}', LocaliteShow::class)->name('circuit-court.localite.show');
-    Route::get('/a-propos', AboutPage::class)->name('circuit-court.about');
-});
+Route::get('/', MapPage::class)->name('circuit-court.map');
+Route::get('/acteur', ActeurIndex::class)->name('circuit-court.acteurs');
+Route::get('/acteur/{shop:slug}', ActeurShow::class)->name('circuit-court.acteur.show');
+Route::get('/filiere', FiliereIndex::class)->name('circuit-court.filieres');
+Route::get('/filiere/{tag:slug}', FiliereShow::class)->name('circuit-court.filiere.show');
+Route::get('/localite', LocaliteIndex::class)->name('circuit-court.localites');
+Route::get('/localite/{city}', LocaliteShow::class)->name('circuit-court.localite.show');
+Route::get('/a-propos', AboutPage::class)->name('circuit-court.about');
